41bd32701f4e54e0d8c2d0c4c38bb6057459bbe7
Arigbo Jesse <arigbojesse@gmail.com>
2025-11-17 12:50:34 +0000

    create the forget password flow

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
             <div className="grid gap-2">
                 <div className="flex items-center">
                     <Label htmlFor="password">Password</Label>
-                    <Link href="#" className="ml-auto inline-block text-sm underline">
+                    <Link href="/auth/forgot-password" className="ml-auto inline-block text-sm underline">
                         Forgot your password?
                     </Link>
                 </div>
