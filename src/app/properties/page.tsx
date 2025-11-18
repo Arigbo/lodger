5a81a2c575273c7c0131962596cbf3e5a287e763
Arigbo Jesse <arigbojesse@gmail.com>
2025-11-18 10:50:41 +0000

    also the public properties single page

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,16 +1,5 @@
+import { redirect } from 'next/navigation';
 
-'use client';
-
-import { notFound } from "next/navigation";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-// This page is deprecated. Redirecting to the student properties search.
 export default function DeprecatedPropertiesPage() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace('/student/properties');
-  }, [router]);
-
-  return notFound();
+  redirect('/student/properties');
 }
